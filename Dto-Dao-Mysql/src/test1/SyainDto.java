6889205db2918b08de4a718dd2241ca0e8e1d9b1
ut23405 <ut23405@gmail.com>
2021-07-18 04:37:40 +0900

    dto-dao-mysql

---
+++ Dto-Dao-Mysql/src/test1/SyainDto.java
@@ -0,0 +1,26 @@
+package test1;
+
+public class SyainDto {
+    private int id;
+    private String name;
+	private String romaji;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRomaji() {
+		return romaji;
+	}
+	public void setRomaji(String romaji) {
+		this.romaji = romaji;
+	}
+}
